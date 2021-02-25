6e77f984358311184be0f01d3b3e3c881dea7b2b
Lameck1 <lameck72@gmail.com>
2021-02-25 14:46:33 +0300

    Add Validation, Associations and Scope for Event
    - Added validations for title, schedule, and description
    - Added appropriate associations
    - Added scope that define future events and past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,14 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :schedule, presence: true
+  validates :description, presence: true, length: { maximum: 100 }
+
+  belongs_to :creator, class_name: 'User'
+
+  has_many :event_attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendances
+
+  scope :past_events, -> { where('schedule < ?', Date.today) }
+  scope :upcoming_events, -> { where('schedule >= ?', Date.today) }
+  
 end
