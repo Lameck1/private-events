be9835b490e2ed2e328ef8e2f4069609833ed577
Lameck1 <lameck72@gmail.com>
2021-02-25 14:20:59 +0300

    Add Validation and Associations to User
    Added validation to username and appropriate associations that a User
    has have Event and Attendace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
+  has_many :event_attendance, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_attendance
 end
