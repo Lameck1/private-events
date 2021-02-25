b27f2b88b1ca88d7128d373881799e819f6f2572
Lameck1 <lameck72@gmail.com>
2021-02-25 14:41:22 +0300

    Refactor Validation and Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { maximum: 10 }
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
-  has_many :event_attendance, foreign_key: :attendee_id
-  has_many :attended_events, through: :event_attendance
+  has_many :event_attendances, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_attendances
 end
