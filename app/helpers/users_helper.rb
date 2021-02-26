eb60f68d2021213580ce8ed004ef61b9a85138d6
Lameck1 <lameck72@gmail.com>
2021-02-26 17:32:33 +0300

    Add User helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,25 @@
 module UsersHelper
+  def user
+    User.find(session[:user_id])
+  end
+
+  def display_past_events(user)
+    if user.attended_events.past_events.empty?
+      render partial: 'users/alert', locals: { text: 'NO PAST EVENTS' }
+    else
+      user.attended_events.past_events.each do |event|        
+        return render partial: 'layouts/event', locals: { event: event }
+      end
+    end
+  end
+
+  def display_upcoming_events(user)
+    if user.attended_events.upcoming_events.empty?
+      render partial: 'users/alert', locals: { text: 'NO UPCOMING EVENTS' }
+    else
+      user.attended_events.upcoming_events.each do |event|
+        return render partial: 'layouts/event', locals: { event: event }
+      end
+    end
+  end
 end
